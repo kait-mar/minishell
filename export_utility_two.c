13295f25e3bb437b03211c9b0e976da06cbc9e36
The-H3ll <44553130+The-H3ll@users.noreply.github.com>
2020-12-12 18:56:16 +0100

    kait_count

--- export_utility_two.c
+++ export_utility_two.c
@@ -6,7 +6,7 @@
 /*   By: molabhai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 18:01:31 by molabhai          #+#    #+#             */
-/*   Updated: 2020/12/10 13:40:32 by molabhai         ###   ########.fr       */
+/*   Updated: 2020/12/12 18:47:00 by molabhai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,3 +37,47 @@ char	*front_append(char *string, char *to_append)
 	s[i] = '\0';
 	return (s);
 }
+
+
+int		kait_count(char *str)
+{
+	int i;
+	int	j;
+	int k;
+	int	count;
+
+	i = 0;
+	k = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (ft_isalnum(str[i]))
+		{
+			j = i;
+			while (((ft_isalnum(str[j]) == 1 ) || str[j] == '=') &&
+					(check_single_double_quote(str[j]) == 0))
+				j += 1;
+			count += 1;
+			i = j;
+		}
+		if (check_double_quotes(str[i]))
+		{
+			j = i + 1;
+			while (check_double_quotes(str[j]) == 0)
+				j += 1;
+			count += 1;
+		   i = j;
+		}
+		if (check_single_quotes(str[i]))
+		{
+			j = i + 1;
+			while (check_single_quotes(str[j]) == 0)
+				j += 1;
+			count += 1;
+		   i = j;
+		}
+		if (str[i] != '\0')
+			i += 1;
+	}
+	return (count);
+}	
