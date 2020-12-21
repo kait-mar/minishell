00e289527fa90252000ead8275ed4b7542c9b66e
Khaoula Ait maryam <kait-mar@student.1337.ma>
2020-12-21 18:27:23 +0100

    echo is fixed

--- echo.sh
+++ echo.sh
@@ -7,3 +7,15 @@ ech"o" hello
 ech'o' hello
 ec"ho" hello
 ech"""o""" hello
+echo
+echo '$PATH'
+echo "$PAtH"
+echo "hello 'world'"
+echo hello $
+echo 'hello "world"'
+echo this "is "a tes't'
+echo hello"my"friend
+echo I 'like'$PATH
+echo mol"abh"'ai'ri
+echo "hello $mouad"
+echo ... 
