2e7c50ad8b2b3ce84a38a90b8bb502a5326442e3
The-H3ll <molabhai@student.1337.ma>
2021-05-31 12:50:23 +0100

    First Commit

--- main_meta/main_helper.c
+++ main_meta/main_helper.c
@@ -108,8 +108,8 @@ void	minishell(char **av, char **env, t_assen assen)
 	while (TRUE)
 	{
 		signal_handler(g_global.status);
-		if (av[1])
-			str = ft_strdup(av[1]);
+		if (av[2])
+			str = ft_strdup(av[2]);
 		else
 		{
 			prompt();
